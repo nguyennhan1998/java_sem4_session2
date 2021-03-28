6d59ad60b6b053336f2e33da5e4cbb110e91d491
nguyennhan1998 <53172541+nguyennhan1998@users.noreply.github.com>
2021-03-28 10:31:55 +0700

    bai tap session 2

--- src/main/java/com/example/crud_user/controller/UserController.java
+++ src/main/java/com/example/crud_user/controller/UserController.java
@@ -2,12 +2,16 @@ package com.example.crud_user.controller;
 
 import com.example.crud_user.entity.User;
 import com.example.crud_user.repository.UserRepository;
+import org.dom4j.rule.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class UserController {
@@ -17,6 +21,30 @@ public class UserController {
     public String index(Model model){
         List<User> users = (List<User>) userRepository.findAll();
         model.addAttribute("users",users);
+        return "index";
+
+    }
+    @RequestMapping(value = "add")
+    public String addUser(Model model){
+        model.addAttribute("user", new User());
+        return  "addUser";
+    }
+    @RequestMapping(value ="/save", method = RequestMethod.POST)
+    public String save(User user){
+        userRepository.save(user);
+        return "redirect:/";
+
+    }
+    @RequestMapping(value = "/edit", method = RequestMethod.POST)
+    public String editUser(@RequestParam("id") Long userID, Model model){
+        Optional<User> userEdit = userRepository.findById(userID);
+        userEdit.ifPresent(user ->model.addAttribute("user",user));
+        return  "editUser";
+    }
+    @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+    public String deleteUser(@RequestParam("id") Long userId, Model model){
+        userRepository.deleteById(userId);
+        return  "redirect:/";
     }
 
 }
